0ec26bf640f5a48e0c055aa85616b386e598cf5e
Nils Büscher <nils-buescher@gmx.de>
2016-11-01 16:20:26 +0100

    updates savefile upgrader

--- Sources/PlanetarySurfaceStructures/SaveFileUpgrader.cs
+++ Sources/PlanetarySurfaceStructures/SaveFileUpgrader.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 namespace PlanetarySurfaceStructures
 {
-    [UpgradeModule(LoadContext.SFS | LoadContext.Craft, sfsNodeUrl = "GAME/FLIGHTSTATE/VESSEL",
-        craftNodeUrl = "VESSEL")]
+    [UpgradeModule(LoadContext.SFS | LoadContext.Craft, sfsNodeUrl = "GAME/FLIGHTSTATE/VESSEL/PART", craftNodeUrl = "PART")]
     public class SaveFileUpgrader : UpgradeScript
     {
+        bool DEBUG = false;
+
         public override string Name
         {
             get
             {
-                Debug.Log("[KPBS] Upgrader: Getting NAME");
                 return "Planetary Base Systems Stack-Node Upgrader";
             }
         }
@@ -21,17 +21,20 @@ namespace PlanetarySurfaceStructures
         {
             get
             {
-                Debug.Log("[KPBS] Upgrader: Getting Description");
                 return "Upgrades the names of attachment nodes from some KPBS parts to fix a bug that causes the nodes to disappear";
             }
+        }
+
+        protected override bool CheckMaxVersion(Version v)
+        {
+            return v <= TargetVersion;
         }
 
         public override Version EarliestCompatibleVersion
         {
             get
             {
-                Debug.Log("[KPBS] Upgrader: EarliestCompatibleVersion");
-                return new Version(1, 0, 0);
+                return new Version(1, 0, 4);
             }
         }
 
@@ -39,64 +42,159 @@ namespace PlanetarySurfaceStructures
         {
             get
             {
-                Debug.Log("[KPBS] Upgrader: TargetVersion");
-                return new Version(1, 1, 0);
+                return new Version(1, 2, 0);
             }
         }
 
         public override TestResult OnTest(ConfigNode node, LoadContext loadContext, ref string nodeName)
         {
-            //node.name
-            nodeName = NodeUtil.GetPartNodeName(node, loadContext);
-
-            Debug.Log("[KPBS] NodeUtil.GetPartNodeName: " + nodeName + " " + node.name);
-
-            TestResult testResult = TestResult.Pass;
-            /*ConfigNode[] nodes = node.GetNodes("PART");
-            TestResult result;
-            int num;
-            for (int i = nodes.Length - 1; i >= 0; i = num)
+            string partName = NodeUtil.GetPartNodeName(node, loadContext).Split('_')[0];
+            string[] attachementNodes = node.GetValues("attN");
+            switch (partName)
             {
-                string value = nodes[i].GetValue("name");
-                string a = value;
-                if (a == "ModuleAmpYearPPTRCS" || a == "ModuleAmpYearPoweredRCS")
-                {
-                    result = TestResult.Upgradeable;
-                    return result;
-                }
-                num = i - 1;
-            }*/
-            //result = testResult;
-            return testResult;
+                case "KKAOSS.Storage.g":
+                case "KKAOSS.Storage.mid.g":
+                case "KKAOSS.Storage.size2.m":
+                case "KKAOSS.Storage.size2.s":
+                    for (int j = 0; j < attachementNodes.Length; j++)
+                    {
+                        string[] values = attachementNodes[j].Split(',');
+                        if ((values[0] == "left") || (values[0] == "right"))
+                        {
+                            return TestResult.Upgradeable;
+                        }
+                    }
+                    break;
+                case "KKAOSS.Service.g":
+                    for (int j = 0; j < attachementNodes.Length; j++)
+                    {
+                        string[] values = attachementNodes[j].Split(',');
+                        if (values[0] == "inner")
+                        {
+                            return TestResult.Upgradeable;
+                        }
+                    }
+                    break;
+                case "KKAOSS.gangway.2.adapter":
+                case "KKAOSS.garage.front.g.2":
+                case "KKAOSS.garage.struct.g.2":
+                    for (int j = 0; j < attachementNodes.Length; j++)
+                    {
+                        string[] values = attachementNodes[j].Split(',');
+                        if (values[0].StartsWith("node_leg"))
+                        {
+                            return TestResult.Upgradeable;
+                        }
+                    }
+                    break;
+                case "KKAOSS.container.SEP":
+                    for (int j = 0; j < attachementNodes.Length; j++)
+                    {
+                        string[] values = attachementNodes[j].Split(',');
+                        if (values[0] == "front")
+                        {
+                            return TestResult.Upgradeable;
+                        }
+                    }
+                    break;  
+            }
+            return TestResult.Pass;
         }
 
         public override void OnUpgrade(ConfigNode node, LoadContext loadContext)
         {
-            Debug.Log("[KPBS] OnUpgrade.GetPartNodeName");
+            string partName = NodeUtil.GetPartNodeName(node, loadContext).Split('_')[0];
+            string[] attachementNodes = node.GetValues("attN");
 
-            /*
-            ConfigNode[] nodes = node.GetNodes("PART");
-            
-
-            int num;
-            for (int i = nodes.Length - 1; i >= 0; i = num)
+            switch (partName)
             {
-                string value = nodes[i].GetValue("name");
-                string a = value;
-                if (a == "ModuleAmpYearPPTRCS")
-                {
-                    nodes[i].SetValue("name", "ModulePPTPoweredRCS", false);
-                }
-                else
-                {
-                    if (a == "ModuleAmpYearPoweredRCS")
+                case "KKAOSS.Storage.g":
+                case "KKAOSS.Storage.mid.g":
+                case "KKAOSS.Storage.size2.m":
+                case "KKAOSS.Storage.size2.s":
+                    int left = 1;
+                    int right = 1;
+                    for (int i = 0; i < attachementNodes.Length; i++)
                     {
-                        nodes[i].SetValue("name", "ModuleIONPoweredRCS", false);
+                        string[] values = attachementNodes[i].Split(',');
+                        if (values[0] == "left")
+                        {
+                            if (DEBUG)
+                            {
+                                Debug.Log("[KPBS] replacing left node: " + left);
+                            }
+                            node.SetValue("attN", "left" + left + "," + values[1], i, false);
+                            left++;
+                        }
+                        else if (values [0] == "right")
+                        {
+                            if (DEBUG)
+                            {
+                                Debug.Log("[KPBS] replacing right node: " + left);
+                            }
+                            node.SetValue("attN", "right" + right + "," + values[1], i, false);
+                            right++;
+                        }
                     }
-                }
-                num = i - 1;
-            }*/
+                    break;
+                case "KKAOSS.garage.front.g.2":
+                case "KKAOSS.garage.struct.g.2":
+                case "KKAOSS.gangway.2.adapter":
+                    int leg = 1;
+                    for (int i = 0; i < attachementNodes.Length; i++)
+                    {
+                        string[] values = attachementNodes[i].Split(',');
+                        if (values[0].StartsWith("node_leg"))
+                        {
+                            if (DEBUG)
+                            {
+                                Debug.Log("[KPBS] replacing leg: " + leg);
+                            }
+                            node.SetValue("attN", "leg" + leg + "," + values[1], i, false);
+                            leg++;
+                        }
+                    }
+                    break;
+                case "KKAOSS.Service.g":
+                    int inner = 1;
+                    for (int i = 0; i < attachementNodes.Length; i++)
+                    {
+                        string[] values = attachementNodes[i].Split(',');
+                        if (values[0] == "inner")
+                        {
+                            if (DEBUG)
+                            {
+                                Debug.Log("[KPBS] replacing inner: " + inner);
+                            }
+                            if (inner == 1)
+                            {
+                                node.SetValue("attN", "innerbottom" + "," + values[1], i, false);
+                            }
+                            else
+                            {
+                                node.SetValue("attN", "innertop" + "," + values[1], i, false);
+                            }
+                            inner++;
+                        }
+                    }
+                    break;
+                case "KKAOSS.container.SEP":
+                    int front = 1;
+                    for (int i = 0; i < attachementNodes.Length; i++)
+                    {
+                        string[] values = attachementNodes[i].Split(',');
+                        if (values[0] == "front")
+                        {
+                            if (DEBUG)
+                            {
+                                Debug.Log("[KPBS] replacing front: " + front);
+                            }
+                            node.SetValue("attN", "front" + front + "," + values[1], i, false);
+                            front++;
+                        }
+                    }
+                    break;
+            }
         }
-
     }
 }
