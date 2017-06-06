7a0881f2713498e61a5101b5a1dfbc492506cd56
Nils277 <nils-buescher@gmx.de>
2017-06-06 11:14:11 +0200

    Updated version files

--- Sources/PlanetarySurfaceStructures/Properties/AssemblyInfo.cs
+++ Sources/PlanetarySurfaceStructures/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.3.0")]
-[assembly: AssemblyFileVersion("1.4.3.0")]
-[assembly: KSPAssembly("PlanetarySurfaceStructures", 1, 0x1430)]
+[assembly: AssemblyVersion("1.4.4.0")]
+[assembly: AssemblyFileVersion("1.4.4.0")]
+[assembly: KSPAssembly("PlanetarySurfaceStructures", 1, 0x1440)]
